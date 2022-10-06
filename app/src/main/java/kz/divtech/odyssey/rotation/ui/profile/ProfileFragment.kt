fbccdfc54a7c1c9678a1770da706f1bcbf140b69
aibolscorpion <as_aibol@mail.ru>
2022-10-06 10:16:16 +0600

    just another commit

--- app/src/main/java/kz/divtech/odyssey/rotation/ui/profile/ProfileFragment.kt
+++ app/src/main/java/kz/divtech/odyssey/rotation/ui/profile/ProfileFragment.kt
@@ -1,6 +1,24 @@
 package kz.divtech.odyssey.rotation.ui.profile
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kz.divtech.odyssey.rotation.databinding.FragmentIinBinding
+import kz.divtech.odyssey.rotation.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View {
+        val binding = FragmentProfileBinding.inflate(inflater)
+        binding.profileFragment = this
+        return binding.root
+    }
+
+    fun showTermsOfAgreement(){
+        val action = ProfileFragmentDirections.actionProfileFragmentToTermsOfAgreementDialog2()
+        findNavController().navigate(action)
+    }
+
 }
