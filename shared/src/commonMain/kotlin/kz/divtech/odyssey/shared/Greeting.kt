bdf83dc5651aea3b98395660f918f1dd5e32e5d0
aibolscorpion <as_aibol@mail.ru>
2024-01-18 18:28:36 +0600

    Удалил ненужные пример классов в общем модуле

--- shared/src/commonMain/kotlin/kz/divtech/odyssey/shared/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package kz.divtech.odyssey.shared
-
-class Greeting {
-    private val platform: Platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
